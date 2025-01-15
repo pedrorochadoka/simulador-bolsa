dfb7003e22318f8e4cdaf362303c19c4a6843a68
Pedro Rocha <pedrorochadoka@gmail.com>
2025-01-14 22:15:23 -0300

    ajuste cors acesso backend

--- projeto-mali-api/app.js
+++ projeto-mali-api/app.js
@@ -6,8 +6,30 @@ const routes = require('../projeto-mali-api/src/routes');
 const app = express();
 const PORT = 3000;
 
+const allowedOrigins = [
+  'https://pedrorochadoka.github.io', // Origem do seu frontend
+];
+
 // Middlewares globais
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    // Permite requisições sem origem (e.g., ferramentas locais como Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://pedrorochadoka.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 // Servir arquivos estáticos (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, '../public')));
 // Servir imagens
